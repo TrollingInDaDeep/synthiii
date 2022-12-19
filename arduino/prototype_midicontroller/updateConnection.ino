c3096379bc38eebb91d6bdf6389e60e586d5a462
TrollingInDaDeep <dorieder@gmx.ch>
2022-12-19 23:56:13 +0100

    continuing drafting the arduino code

---
+++ arduino/prototype_midicontroller/updateConnection.ino
@@ -0,0 +1,28 @@
+/*
+ * Handles updates in connections
+ * Status of every possible connecion is stored in
+ * arrConnectionStates [][] -> defined in main file (prototype_midicontroller)
+ *  
+ */
+
+/// updates connection status in pre-stored array
+/// afterwards sends connection update via midi
+void updateConnection(int Cin, int Cout, bool blnConnected)
+ {
+  if (arrConnectionStates[Cin][Cout] != blnConnected)
+    {
+      arrConnectionStates[Cin][Cout] == blnConnected;
+
+      //connect if true, disconnect if false
+      // todo: define how this actually works
+      if (blnConnected)
+      {
+        sendMIDI(statusByteMakeConnection, Cin, Cout);
+      }
+      else
+      {
+        sendMIDI(statusByteBreakConnection, Cin, Cout);
+      }
+      
+    }
+ }
