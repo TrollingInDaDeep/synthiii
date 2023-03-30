e784b58cc7c832c121a0fec07bf308b5c52ecf44
TrollingInDaDeep <dorieder@gmx.ch>
2023-03-30 23:03:59 +0200

    I am awesome!!! Main issue was that ground was often not connected, so floating pins. secondly if ground was connected, sometimes the connection wasn't as good. this led to the pot value readings never going down to zero. that's why I am now resoldering the +5v and GND wires to a rail. Also on the proto panel, some POTs are probably broken so need to replace them or exclude. However: I can now read 8 POTS simultaneously via one Multiplexer!!! so happy! Also could connect 3 LEDs to one multiplexer as Dout. First on step 8, all LEDs were on instead of none, this issue was gone after re-grounding all the unused pins. ok now I need to go to bed.

--- arduino/shield_v1/shield_v1.ino
+++ arduino/shield_v1/shield_v1.ino
@@ -10,8 +10,8 @@
 // Detailed pinout:
 // Mux 1
 // I/O -> A0
-// C   -> 0
-// B   -> 1
+// C   -> 7
+// B   -> 8
 // A   -> 2
 
 // MUX2
@@ -21,21 +21,23 @@
 // A   -> 5
 
 // Analog Pins
-  const byte Ain = A0;
+  const int Ain = A5;
 
 
 // Digital Pins
   const int Din = 6;
+  const int Dout = 8;
   const int T1_A = 2;
-  const int T1_B = 1;
-  const int T1_C = 0;
+  const int T1_B = 3;
+  const int T1_C = 4;
   const int T2_A = 5;
-  const int T2_B = 4;
-  const int T2_C = 3;
+  const int T2_B = 6;
+  const int T2_C = 7;
 
 // var declarations to use later
 // stores current state/value of inputs/outputs/mux control pins
   int intCurrAin;
+  bool blnCurrDin;
   bool blnCurrT1_A;
   bool blnCurrT1_B;
   bool blnCurrT1_C;
@@ -63,9 +65,15 @@ void setup() {
   //set pin modes
   //Analog
   pinMode(Ain, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
 
   //Digital
   pinMode(Din,INPUT_PULLUP);
+  pinMode(Dout,OUTPUT);
   pinMode(T1_A,OUTPUT);
   pinMode(T1_B,OUTPUT);
   pinMode(T1_C,OUTPUT);
@@ -78,60 +86,46 @@ void setup() {
 }
 
 void loop() {
+   digitalWrite(Dout, HIGH);
+  // digitalWrite(T2_A, 0);
+  // digitalWrite(T2_B, 0);
+  // digitalWrite(T2_C, 0);
+  // delay(100);
+  // intCurrAin = analogRead(Ain);
+  // Serial.println(intCurrAin);
 
-    
-  digitalWrite(T1_A, 0);
-  digitalWrite(T1_B, 0);
-  digitalWrite(T1_C, 0);
-  delay(100);
-  intCurrAin = analogRead(A0);
-  Serial.println(intCurrAin);
-  delay(500);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // delay(500);
 
   //reset input/output indexes
-  //intInputNr = 0;
+  intInputNr = 0;
 
-  // put your main code here, to run repeatedly:
-  // for(int T1=0;T1<8;T1++){
-  //   intInputNr++;
+  for(int T1=0;T1<8;T1++){
+    intInputNr++;
 
-  //   //use T1 for Ain L1
-  //   blnCurrT1_A = bitRead(T1, 0); //LSB
-  //   blnCurrT1_B = bitRead(T1, 1);
-  //   blnCurrT1_C = bitRead(T1, 2); //MSB
-  //   digitalWrite(T1_A, blnCurrT1_A);
-  //   digitalWrite(T1_B, blnCurrT1_B);
-  //   digitalWrite(T1_C, blnCurrT1_C);
-  //   delay(10);
-  //   Serial.print(blnCurrT1_C);
-  //   Serial.print(blnCurrT1_B);
-  //   Serial.print(blnCurrT1_A);
+    //use T1 for Ain L1
+    blnCurrT1_A = bitRead(T1, 0); //LSB
+    blnCurrT1_B = bitRead(T1, 1);
+    blnCurrT1_C = bitRead(T1, 2); //MSB
+    digitalWrite(T1_A, blnCurrT1_A);
+    digitalWrite(T1_B, blnCurrT1_B);
+    digitalWrite(T1_C, blnCurrT1_C);
 
-  //   // Read analog value and send it to Analog function.
-  //   intCurrAin = analogRead(Ain);
-  //   readAin(intCurrAin, intInputNr);
-  //   Serial.print("-");
-  //   Serial.print(intInputNr);
-  //   Serial.print(":");
-  //   Serial.println(intCurrAin);
-  //   delay(500);
-  // }
 
+    blnCurrT2_A = bitRead(T1, 0); //LSB
+    blnCurrT2_B = bitRead(T1, 1);
+    blnCurrT2_C = bitRead(T1, 2); //MSB
+    digitalWrite(T2_A, blnCurrT2_A);
+    digitalWrite(T2_B, blnCurrT2_B);
+    digitalWrite(T2_C, blnCurrT2_C);
+    delay(1);
+
+    // Read analog value and send it to Analog function.
+    intCurrAin = analogRead(Ain);
+    readAin(intCurrAin, intInputNr);
+    Serial.print(intCurrAin);
+    //Serial.print(intInputNr);
+    Serial.print("\t");
+  }
+  Serial.println("");
+  delay(5);
 }
