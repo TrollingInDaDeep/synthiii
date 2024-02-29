a1e39d5be148fff378c2aaee0fbdcc733aa531ab
TrollingInDaDeep <dorieder@gmx.ch>
2024-03-01 00:21:20 +0100

    ITWORKS!! io checker script using all inputs (digital and analog)

--- teensy/IO_Mux_check/IO_Mux_check.ino
+++ teensy/IO_Mux_check/IO_Mux_check.ino
@@ -4,12 +4,12 @@
 // Pins
 const int I1 = 38;    //Analog In -> synth rechts
 const int I2 = 41;    //Analog In -> synth links
-const int I3 = 40;    //Analog In
-const int I4 = 39;    //Analog In
-const int I5 = 25;    //Analog In
-const int I6 = 26;    //Analog In
-const int I7 = 11;    //Digital In
-const int I8 = 12;    //Digital Out
+const int I3 = 40;    //Analog In -> seq links pot
+const int I4 = 39;    //Digital In -> seq links btn
+const int I5 = 25;    //Analog In -> seq faders
+const int I6 = 26;    //Analog In -> seq pulse
+const int I7 = 11;    //Digital Out -> seq LEDs
+const int I8 = 12;    //Digital In -> seq btn triggers
 const int A = 37;     //Digital Out
 const int B = 36;     //Digital Out
 const int C = 35;     //Digital Out
@@ -21,32 +21,93 @@ const int Cout = 32;  //Digital Out
 
 // Settings
 const int pinPerMux = 8; // 4051 Multiplexer has 8 pins
-const int numAnalogMux = 5; // how many analog multiplexers
+const int numAnalogInMux = 5; // how many analog multiplexers
+const int numDigitalInMux = 2; // how many analog multiplexers
+
+
+//vars
+int rawAnalog = 0;
+bool rawDigital = 0;
 
 ///
 /// Arrays
 ///
 
-// int arr_prev_read_analog_inputs [numAnalogMux][pinPerMux] {
-//   {0, 0, 0, 0, 0, 0, 0, 0}; //I1
-//   {0, 0, 0, 0, 0, 0, 0, 0}; //
-// };
+// pin numbers of analog inputs
+int arr_pin_analog_inputs [numAnalogInMux] {
+  I1,
+  I2,
+  I3,
+  I5,
+  I6
+};
 
-// previous read value to reduce flickering
-int prev_read_I1 [pinPerMux] = {0, 0, 0, 0, 0, 0, 0, 0};
-int prev_read_I2 [pinPerMux] = {0, 0, 0, 0, 0, 0, 0, 0};
+// pin numbers of digital inputs
+int arr_pin_digital_inputs [numAnalogInMux] {
+  I4,
+  I8
+};
 
-// current read value
-int read_I1 [pinPerMux] = {0, 0, 0, 0, 0, 0, 0, 0};
-int read_I2 [pinPerMux] = {0, 0, 0, 0, 0, 0, 0, 0};
+// previous reading of analog inputs
+int arr_prev_read_analog_inputs [numAnalogInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I1
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I2
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I3
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I5
+  {0, 0, 0, 0, 0, 0, 0, 0} //I6
+};
 
+// previous reading of digital inputs
+int arr_prev_read_digital_inputs [numDigitalInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I4
+  {0, 0, 0, 0, 0, 0, 0, 0} //I8
+};
+
+// current reading of analog inputs
+int arr_read_analog_inputs [numAnalogInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I1
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I2
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I3
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I5
+  {0, 0, 0, 0, 0, 0, 0, 0} //I6
+};
+
+// current reading of digital inputs
+int arr_read_digital_inputs [numDigitalInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I4
+  {0, 0, 0, 0, 0, 0, 0, 0} //I8
+};
+
+// midi/CC to send for analog inputs
+int arr_send_analog_inputs [numAnalogInMux][pinPerMux] {
+  {1, 2, 3, 4, 5, 6, 7, 8}, //I1
+  {9, 10, 11, 12, 13, 14, 15, 16}, //I2
+  {17, 18, 19, 20, 21, 22, 23, 24}, //I3
+  {25, 26, 27, 28, 29, 30, 31, 32}, //I5
+  {33, 34, 35, 36, 37, 38, 39, 40} //I6
+};
+
+// midi/CC to send for digital inputs
+int arr_send_digital_inputs [numDigitalInMux][pinPerMux] {
+  {41, 42, 43, 44, 45, 46, 47, 48}, //I4
+  {49, 50, 51, 52, 53, 54, 55, 56} //I8
+};
 
 // Masks to enable/disable single inputs/outputs
 // Used to ignored non-connected inputs/outputs
 // 1 = disabled
-bool disable_I1 [pinPerMux] = {0, 0, 0, 0, 0, 0, 1, 1};
-bool disable_I2 [pinPerMux] = {0, 0, 0, 0, 0, 0, 0, 0};
+bool arr_disable_analog_inputs [numAnalogInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 1, 1}, //I1
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I2
+  {0, 0, 0, 0, 1, 1, 1, 1}, //I3
+  {0, 0, 0, 0, 0, 0, 0, 0}, //I5
+  {0, 0, 0, 0, 0, 0, 0, 0} //I6
+};
 
+bool arr_disable_digital_inputs [numDigitalInMux][pinPerMux] {
+  {0, 0, 0, 0, 0, 0, 1, 1}, //I4
+  {0, 0, 0, 0, 0, 0, 0, 0} //I8
+};
 
 ///
 /// Functions
@@ -77,52 +138,56 @@ void selectMuxOutPin(byte pin){
 
 
 void readAnalogPins() {
-  
-  // read multiplexer
-  for (byte pin=0; pin<=7; pin++){
-    selectMuxInPin(pin);
-    int raw_I1;
-    if (!disable_I1[pin]){
-      raw_I1 = analogRead(I1);
-      prev_read_I1[pin] = read_I1[pin];
-      read_I1[pin] = map(raw_I1,0,1023,0,127);
-    }
+  // loop through analog in multiplexers
+  for (int mux = 0; mux<numAnalogInMux; mux++){
     
-    if (prev_read_I1[pin] != read_I1[pin]) {
-      Serial.print(pin);
-      Serial.print(" reads: ");
-      Serial.println(raw_I1);
-      usbMIDI.sendControlChange(1 + pin, read_I1[pin], 1);
-    }
-
-
-
-    int raw_I2;
-    if (!disable_I2[pin]){
-      raw_I2 = analogRead(I2);
-      prev_read_I2[pin] = read_I2[pin];
-      read_I2[pin] = map(raw_I2,0,1023,0,127);
-    }
-    
-    if (prev_read_I2[pin] != read_I2[pin]) {
-      Serial.print(pin);
-      Serial.print(" reads: ");
-      Serial.println(raw_I2);
-      usbMIDI.sendControlChange(10 + pin, read_I1[pin], 1);
-    }
+    //loop through every pin for the analog muxes
+    for (byte pin=0; pin<=7; pin++){
+        selectMuxInPin(pin);
+        // only read if input is not disabled
+        if (!arr_disable_analog_inputs[mux][pin]){
+          rawAnalog = analogRead(arr_pin_analog_inputs[mux]);
+          arr_prev_read_analog_inputs[mux][pin] = arr_read_analog_inputs[mux][pin];
+          arr_read_analog_inputs[mux][pin] = map(rawAnalog,0,1023,0,127);
+        }
+        
+        if (arr_prev_read_analog_inputs[mux][pin] != arr_read_analog_inputs[mux][pin]) {
+          Serial.print("mux ");
+          Serial.print(mux);
+          Serial.print(" pin ");
+          Serial.print(pin);
+          Serial.print(" reads: ");
+          Serial.println(arr_read_analog_inputs[mux][pin]);
+          usbMIDI.sendControlChange(arr_send_analog_inputs[mux][pin], arr_read_analog_inputs[mux][pin], 1);
+        }
+      }
   }
 }
 
 void readDigitalPins() {
-  
-  // read multiplexer
-  for (byte pin=0; pin<=7; pin++){
-    selectMuxInPin(pin);
-    bool readRaw = digitalRead(I7);
-    //muxValues[pin] = map(seqRaw,0,1023,0,85);
-    Serial.print(pin);
-    Serial.print(" reads: ");
-    Serial.println(readRaw);
+  // loop through digital in multiplexers
+  for (int mux = 0; mux<numDigitalInMux; mux++){
+    
+    //loop through every pin for the digital muxes
+    for (byte pin=0; pin<=7; pin++){
+        selectMuxInPin(pin);
+        // only read if input is not disabled
+        if (!arr_disable_digital_inputs[mux][pin]){
+          rawDigital = digitalRead(arr_pin_digital_inputs[mux]);
+          arr_prev_read_digital_inputs[mux][pin] = arr_read_digital_inputs[mux][pin];
+          arr_read_digital_inputs[mux][pin] = rawDigital;
+        }
+        
+        if (arr_prev_read_digital_inputs[mux][pin] != arr_read_digital_inputs[mux][pin]) {
+          Serial.print("mux ");
+          Serial.print(mux);
+          Serial.print(" pin ");
+          Serial.print(pin);
+          Serial.print(" reads: ");
+          Serial.println(arr_read_digital_inputs[mux][pin]);
+          usbMIDI.sendControlChange(arr_send_digital_inputs[mux][pin], arr_read_digital_inputs[mux][pin], 1);
+        }
+      }
   }
 }
 
@@ -130,12 +195,12 @@ void readDigitalPins() {
 void setup() {
   pinMode(I1, INPUT);
   pinMode(I2, INPUT);
-  //pinMode(I3, INPUT);
-  //pinMode(I4, INPUT);
-  //pinMode(I5, INPUT);
-  //pinMode(I6, INPUT);
-  //pinMode(I7, INPUT_PULLUP);
-  //pinMode(I8, OUTPUT);
+  pinMode(I3, INPUT);
+  pinMode(I4, INPUT_PULLUP);
+  pinMode(I5, INPUT);
+  pinMode(I6, INPUT);
+  //pinMode(I7, OUTPUT);
+  pinMode(I8, INPUT_PULLUP);
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(C, OUTPUT);
@@ -146,4 +211,5 @@ void setup() {
 
 void loop() {
   readAnalogPins();
+  readDigitalPins();
 }
